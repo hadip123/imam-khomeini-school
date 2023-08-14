7a72f576e43ccd60205094ad03d74aa055df7396
Hadi Pooya <mhn.hadi@outlook.com>
2023-08-14 10:16:59 +0330

    API

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def update_form():
         return 'Found', 400
 
     # Load the workbook
-    wb = openpyxl.load_workbook('form.xlsx')
+    wb = openpyxl.load_workbook('form2.xlsx')
 
     # Get the worksheet
     ws = wb['form']
@@ -26,11 +26,11 @@ def update_form():
     # Name
     ws['B7'] = request.json['name']
     # LastName
-    ws['J7'] = request.json['last_name'] 
+    ws['I7'] = request.json['last_name'] 
     # Father Name
     ws['M7'] = request.json['father_name']
     # Field of Study
-    ws['Q7'] = request.json['field_of_study']
+    ws['S7'] = request.json['field_of_study']
     # Number of Identity
     ws['X7'] = request.json['id_number']
     # (Alphabet) Number of Identity
@@ -38,67 +38,73 @@ def update_form():
     # (Int) Number of Identity
     ws['AA8'] = request.json['id_number_int']
     # Code Melli
-    ws['H8'] = request.json['national_code']
+    ws['I8'] = request.json['national_code']
     # Place of Issue
     ws['M8'] = request.json['issue_place']
     # Mother Name
-    ws['S8'] = request.json['mother_name']
+    ws['I20'] = request.json['mother_name']
     # (Day) Birth
     ws['D9'] = request.json['birth_day']
     # (Month) Birth
     ws['F9'] = request.json['birth_month']
     # (Year) Birth
     ws['H9'] = request.json['birth_year']
+    # Province
+    ws['L9'] = request.json['province']
     # Country
     ws['P9'] = request.json['country']
     # City/Village
     ws['U9'] = request.json['city']
     # Postal Code
     ws['X9'] = request.json['postal_code']
+    # Din
+    ws['B10'] = request.json['din']
     # Religion
-    ws['H10'] = request.json['religion']
+    ws['F10'] = request.json['religion']
+    # Nationality
+    ws['J10'] = request.json['nationality']
     # Status of body
     ws['N10'] = request.json['body_status'] 
     # Mobile number in "Shad" system
     ws['R10'] = request.json['shad_mobile']
     # Is Left-Handed?
-    ws['Z10'] = request.json['left_handed']
+    ws['Z11'] = request.json['left_handed']
     # Father's education
-    ws['F12'] = request.json['father_education']
+    ws['T18'] = request.json['father_education']
     # Father's occupation
-    ws['K12'] = request.json['father_occupation']
+    ws['X18'] = request.json['father_occupation']
     # Father's work address
-    ws['O12'] = request.json['father_work_address']
+    ws['L19'] = request.json['father_work_address']
     # Father's work phone
-    ws['X12'] = request.json['father_work_phone']
+    ws['X19'] = request.json['father_work_phone']
     # (Day) Father's Birth
-    ws['F13'] = request.json['father_birth_day']
+    ws['F19'] = request.json['father_birth_day']
     # (Month) Father's Birth  
-    ws['H13'] = request.json['father_birth_month']
+    ws['H19'] = request.json['father_birth_month']
     # (Year) Father's Birth
-    ws['J13'] = request.json['father_birth_year']
+    ws['J19'] = request.json['father_birth_year']
     # Father's ID
-    ws['L13'] = request.json['father_id']
+    ws['Q18'] = request.json['father_id']
     # Father's Place of Issue
-    ws['O13'] = request.json['father_issue_place'] 
+    ws['Q19'] = request.json['father_issue_place'] 
     # Father's Insurance code
-    ws['S13'] = request.json['father_insurance_code']
+    ws['T19'] = request.json['father_insurance_code']
     # Father's National Code
-    ws['X13'] = request.json['father_national_code']
+    ws['M18'] = request.json['father_national_code']
     # Mother's education
-    ws['F14'] = request.json['mother_education']
+    ws['T20'] = request.json['mother_education']
     # Mother's occupation
-    ws['K14'] = request.json['mother_occupation']
+    ws['X20'] = request.json['mother_occupation']
     # Mother's work address
-    ws['O14'] = request.json['mother_work_address']
+    ws['L21'] = request.json['mother_work_address']
     # Mother's work phone
-    ws['X14'] = request.json['mother_work_phone']
+    ws['X21'] = request.json['mother_work_phone']
     # (Day) Mother's Birth
-    ws['F15'] = request.json['mother_birth_day']
+    ws['F21'] = request.json['mother_birth_day']
     # (Month) Mother's Birth
-    ws['H15'] = request.json['mother_birth_month']
+    ws['H21'] = request.json['mother_birth_month']
     # (Year) Mother's Birth
-    ws['J15'] = request.json['mother_birth_year']  
+    ws['J21'] = request.json['mother_birth_year']  
 
     # (Day) Supervisor's Birth
     ws['F21'] = request.json['supervisor_birth_day']
@@ -107,73 +113,97 @@ def update_form():
     # (Year) Supervisor's Birth
     ws['J21'] = request.json['supervisor_birth_year']
 
+    # Supervisor name
+    ws['I22'] = request.json['supervisor_name']
+    # supervisor's education
+    ws['T22'] = request.json['supervisor_education']
+    # supervisor's occupation
+    ws['X22'] = request.json['supervisor_occupation']
+    # supervisor's work address
+    ws['L23'] = request.json['supervisor_work_address']
+    # supervisor's work phone
+    ws['X19'] = request.json['supervisor_work_phone']
+    # (Day) supervisor's Birth
+    ws['F23'] = request.json['supervisor_birth_day']
+    # (Month) supervisor's Birth  
+    ws['h23'] = request.json['supervisor_birth_month']
+    # (Year) supervisor's Birth
+    ws['J23'] = request.json['supervisor_birth_year']
+    # supervisor's ID
+    ws['Q22'] = request.json['supervisor_id']
+    # supervisor's Place of Issue
+    ws['Q23'] = request.json['supervisor_issue_place'] 
+    # supervisor's Insurance code
+    ws['T23'] = request.json['supervisor_insurance_code']
+    # supervisor's National Code
+    ws['M22'] = request.json['supervisor_national_code']
+
 
     # Mother's ID
-    ws['L15'] = request.json['mother_id']
+    ws['Q20'] = request.json['mother_id']
     # Mother's Place of Issue
-    ws['O15'] = request.json['mother_issue_place']
+    ws['Q21'] = request.json['mother_issue_place']
     # Mother's Insurance code
-    ws['S15'] = request.json['mother_insurance_code']
+    ws['T21'] = request.json['mother_insurance_code']
     # Mother's National Code
-    ws['X15'] = request.json['mother_national_code']
+    ws['M20'] = request.json['mother_national_code']
     # Address
-    ws['H16'] = request.json['address']
+    ws['H12'] = request.json['address']
     # Home Telephone
-    ws['R16'] = request.json['home_phone']
+    ws['O12'] = request.json['home_phone']
     # Status of housing
-    ws['Y16'] = request.json['housing_status']
+    ws['Y12'] = request.json['housing_status']
     # Father's phone number
-    ws['E17'] = request.json['father_phone']
-    # Mother's phone number
-    ws['L17'] = request.json['mother_phone']  
+    # ws['E17'] = request.json['father_phone']
+    # # Mother's phone number
+    # ws['L17'] = request.json['mother_phone']  
     # Phone number
-    ws['Q17'] = request.json['phone']
+    ws['W10'] = request.json['phone']
     # Emergency phone number
-    ws['W17'] = request.json['emergency_phone']
+    ws['S12'] = request.json['emergency_phone']
     # Who does the student live with at home?
-    ws['J18'] = request.json['live_with']
+    ws['J14'] = request.json['live_with']
     # The student's housing situation if he lives away from his family to study: 
-    ws['T18'] = request.json['housing_situation']
+    ws['T14'] = request.json['housing_situation']
     # Does the student have an independent study room:
-    ws['AA18'] = request.json['study_room']
+    ws['AA14'] = request.json['study_room']
     # the number of family members:
-    ws['E19'] = request.json['family_members']
+    ws['E15'] = request.json['family_members']
     # How many children are there before him?
-    ws['K19'] = request.json['children_before']
+    ws['K15'] = request.json['children_before']
     # Who is the student supervisor?
-    ws['Q19'] = request.json['student_supervisor']
+    ws['Q15'] = request.json['student_supervisor']
     # Email
-    ws['W19'] = request.json['email']
+    ws['V15'] = request.json['email']
     # Height  
-    ws['D20'] = request.json['height']
+    ws['D11'] = request.json['height']
     # Weight
-    ws['K20'] = request.json['weight']
+    ws['K11'] = request.json['weight']
     # Ability, skill and position or rank:
-    ws['P20'] = request.json['ability']
+    ws['P11'] = request.json['ability']
     # The number used in the government portal
-    ws['P21'] = request.json['gov_portal_number']
+    ws['S8'] = request.json['gov_portal_number']
     # Status of pervious year
     ws['H23'] = request.json['previous_year_status']
-    # The total GPA of the ninth grade:
-    ws['P23'] = request.json['ninth_gpa']
-    # The GPA of ninth grade second period
-    ws['U23'] = request.json['ninth_grade_second_gpa']
+    # The total GPA of the ninth grade: (Total GPA)
+    ws['O16'] = request.json['ninth_gpa']
     # Accepted in "Nemoone-Dolati" Test
-    ws['AA23'] = request.json['dolati_test']
+    ws['T16'] = request.json['dolati_test']
     # Pervious school name
-    ws['H24'] = request.json['previous_school_name']  
-    # Pervious school code
-    ws['P24'] = request.json['previous_school_code']
+    ws['I16'] = request.json['previous_school_name']  
     # Witness quota, sacrifice:
-    ws['V24'] = request.json['witness_quota']
+    ws['V13'] = request.json['witness_quota']
     # Under the cover of the Imam Khomeini (RA) relief committee:
-    ws['J25'] = request.json['imam_relief']
+    ws['J13'] = request.json['imam_relief']
     # Under welfare:
-    ws['O25'] = request.json['welfare']
+    ws['M13'] = request.json['welfare']
     # Does father work in Ministry of Education?
-    ws['T25'] = request.json['father_education_ministry']
+    ws['P13'] = request.json['father_education_ministry']
     # Does mother work in Ministry of Education?
-    ws['Y25'] = request.json['mother_education_ministry']
+    ws['S13'] = request.json['mother_education_ministry']
+
+    # Father full name
+    ws['I18'] = request.json['father_name'] + ' ' + request.json['last_name']
 
     # Save workbook
     wb.save('students/' + request.json['national_code'] + '-' + request.json['name'] + '-' + request.json['last_name'] + '-' + request.json['field_of_study'] + '.xlsx')
